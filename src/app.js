5173d074d68150c6fe17f301dfeaeebcc9387ba8
Manuel Pernia <manuelk211995@gmail.com>
2026-02-18 10:52:09 +0000

    nombre dominio hecho

--- src/app.js
+++ src/app.js
@@ -1,11 +1,32 @@
-import "bootstrap";
-import "./style.css";
 
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+
+
+  for (let index = 0; index < pronoun.length; index++) {
+
+    for (let i = 0; i < adj.length; i++) {
+
+      for (let index1 = 0; index1 < noun.length; index1++) {
+
+        console.log(pronoun[index] + adj[i] + noun[index1] + ".com");
+
+
+
+      }
+
+
+
+    }
+
+
+  }
+
+
+
+
+
